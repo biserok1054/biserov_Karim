8279e399a2449a059cc61979275a7acd588775de
biserok1054 <56262207+biserok1054@users.noreply.github.com>
2019-10-23 15:41:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,155 @@
-import java.util.Arrays;
-
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
+    static Scanner Scanner;
     public static void main(String[] args) {
-        int[] arr;
-        arr = new int[5];
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-        arr[3] = 40;
-        arr[4] = 50;
-        for (int i = 0; i < arr.length; i++)
-            System.out.println("b" + i + ":" + arr[i]);
-
-        
-        double[] a;
-        a = new double[5];
-        a[0] = 0;
-        for (int i = 1; i < a.length; i++) {
-            a[i] = a[i - 1] + 0.5;
-            System.out.println(a[i]);
+        Scanner  = new Scanner(System.in);
+        String command = null;
+        Mail mail = null;
+        List<Mail> mails = new ArrayList<Mail>();
+        while(true){
+            command = Scanner.nextLine();
+            if(command.equals("Начать")){
+                mail = createMail();
+                mails.add(mail);
+            }
+            else if(command.equals("просмотреть нахождение товара")){
+                Mail mail1 = findCurrentMail(mails);
+                if(mail1 == null){
+                    System.out.println("Ошибка \\. Попробуйте еще раз");
+                }
+                else {
+                    System.out.println(mail.getOrdernumber());
+                    System.out.println(mail.getOwner());
+                    System.out.println(mail.getWeight());
+                }
+            }
+            else if(command.equals("изменить")){
+                changeOrdernumber(mail);
+            }
         }
-
-
-
     }
 
+    private static Mail createMail() {
+    }
+
+    private static Mail findCurrentMail(List<Mail> mails) {
+    }
+
+    {
+        System.out.println("Введи владельца");
+        String string = Scanner.nextLine();
+        Mail[] mails;
+        for(Mail mail:mails){
+            if(mail.getOwner().equals(string)) {
+                return mail;
+            }
+        }
+        return null;
+    }
+
+    private static void changeOrdernumber(Mail mail) {
+        System.out.println("введи новое описание");
+        String weight = Scanner.nextLine();
+        mail.setWeight(weight);
+    }
+
+    private static Mail createMail() {
+        System.out.println("Введи номер заказа");
+        String ordernumber = Scanner.nextLine();
+        System.out.println("Введи имя получателя");
+        String owner = Scanner.nextLine();
+        Mail mail = new Mail(ordernumber,owner,23, new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis() + 573745734));
+        return mail;
+
+        private static Mail ;createMail();
+    }
+
+    
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
