ee0c66067e38a89bdf95369ae197428c19ff26b7
biserok1054 <56262207+biserok1054@users.noreply.github.com>
2019-11-18 12:28:49 +0300

    Update Main.java

--- Fghhjhg/src/Main.java
+++ Fghhjhg/src/Main.java
@@ -7,16 +7,16 @@ public class Main {
         int [] mas = {92, 85, 1, 744, 54};
 
         boolean isSorted = false;
-        int buf;
+        int g;
         while(!isSorted) {
             isSorted = true;
             for (int i = 0; i < mas.length-1; i++) {
                 if(mas[i] < mas[i+1]){
                     isSorted = false;
 
-                    buf = mas[i];
+                    g = mas[i];
                     mas[i] = mas[i+1];
-                    mas[i+1] = buf;
+                    mas[i+1] = g;
                 }
             }
         }
