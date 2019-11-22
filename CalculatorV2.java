423521a4c4da118d87198815b044bcd0d7316268
biserok1054 <56262207+biserok1054@users.noreply.github.com>
2019-11-22 16:13:29 +0300

    Update CalculatorV2.java

--- CalculatorV2.java
+++ CalculatorV2.java
@@ -2,38 +2,65 @@ import java.util.Scanner;
 
 public class CalculatorV2 {
     public static Scanner scanner;
+
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        int a,b;
+        int a, b;
         System.out.println("Введите каждую цифру по отдельности");
         a = scanner.nextInt();
         b = scanner.nextInt();
-        char g = scanner.nextLine().charAt(0);
+        char operation = getOperation();
+        int result = calc(a, b, operation);
+        System.out.println("Результат операции: " + result);
+    }
+
+    public static int getInt() {
+        System.out.println("Введите число:");
+        int num;
+        if (scanner.hasNextInt()) {
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation() {
+        System.out.println("Введите операцию:");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int num1, int num2, char operation) {
         int result;
-        float result1;
-        if (g == '+') {
-            result = plus(a, b);
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());
         }
-        else if(g == '-'){
-            result = minus(a, b);
-        }
-        else if(g == '*'){
-            result = multiple(a, b);
-        }
-    }
-
-    private static int multiple(int a, int b) {
-        return a * b;
-    }
-
-    private static int minus(int a, int b) {
-        return a - b;
-    }
-
-    private static int plus(int a, int b) {
-        return a + b;
+        return result;
 
     }
-
-
 }
+
